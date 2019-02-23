8c4de6296bc8d15740271ec146ecf3c7aeee1a05
liujidong <wwwliujidong@163.com>
2019-02-23 15:50:27 +0800

    查找Actor另外方法todo

--- src/main/java/LookupActor.java
+++ src/main/java/LookupActor.java
@@ -1,9 +1,16 @@
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
 import akka.actor.ActorIdentity;
+import akka.actor.ActorNotFound;
 import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
+import akka.actor.ActorSystem;
 import akka.actor.Identify;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
+import akka.dispatch.OnFailure;
+import akka.dispatch.OnSuccess;
+import akka.util.Timeout;
 
 
 public class LookupActor extends UntypedActor {
@@ -15,7 +22,26 @@ public class LookupActor extends UntypedActor {
 	public void onReceive(Object msg) throws Exception {
 		if(msg instanceof String){
 			if("find".equals(msg)){
-				ActorSelection as = getContext().actorSelection("targetActor");
+				//ActorSelection as = getContext().actorSelection("targetActor");
+				ActorSystem system = ActorSystem.create("sys");
+				ActorSelection as = system.actorSelection("/user/lookupActor/targetActor");
+				Timeout timeout = new Timeout(Duration.create(20, "seconds"));
+				Future<ActorRef> fu = as.resolveOne(timeout);
+				fu.onSuccess(new OnSuccess<ActorRef>() {
+					@Override
+					public void onSuccess(ActorRef ref) throws Throwable {
+						System.out.println("查找到Actor："+ref);
+					}
+					
+				}, system.dispatcher());
+				fu.onFailure(new OnFailure() {
+					@Override
+					public void onFailure(Throwable ex) throws Throwable {
+						if(ex instanceof ActorNotFound){
+							System.out.println("没找到Actor:"+ex.getMessage());
+						}
+					}
+				}, system.dispatcher());
 				as.tell(new Identify("A001"), getSelf());//发送后会自动接收
 			}
 		}else if(msg instanceof ActorIdentity){
